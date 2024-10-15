5f47d2470b8b225287845ba4a8c9cc67296e6ed5
kim dahye <makamong0000@gmail.com>
2024-10-15 15:15:37 +0900

    20241015

--- src/main/java/org/example/springbootdeveloper/service/StudentService.java
+++ src/main/java/org/example/springbootdeveloper/service/StudentService.java
@@ -15,7 +15,7 @@ public class StudentService {
 
     private final StudentRepository studentRepository;
 
-    // 생성자 주입 (DI)
+    // 생성자 주입(DI)
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
         // 생성자를 통해 의존성 주입
@@ -63,6 +63,66 @@ public class StudentService {
                     HttpStatus.INTERNAL_SERVER_ERROR,
                     "Error occurred while fetching the student", e
             ); // 데이터 조회 중 에러 발생 시 예외 처리
+        }
+    }
+
+    // 3) 새로운 학생 등록
+    public StudentDto createStudent(StudentDto studentDto) {
+        try {
+            Student student = new Student(studentDto.getName(), studentDto.getEmail());
+
+            //생성한 Student 객체를 DB에 저장
+            Student savedStudent = studentRepository.save(student);
+
+            return new StudentDto(savedStudent.getId(), savedStudent.getName(), savedStudent.getEmail());
+        } catch (Exception e){
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    "Failed to create student", e
+            ); //등록 중 에러 발생 시 예외 처리
+        }
+    }
+
+    //4 특정 ID 회원정보 수정
+    public StudentDto updateStudent(Long id, StudentDto studentDto){
+        try {
+            Student student = studentRepository.findById(id)
+                    .orElseThrow(() -> new Error("Student not found whit id" + id));
+                    // 수정할 학생이 없는 경우 예외 발생
+            // 학생 정보 수정
+            student.setName(studentDto.getName());
+            student.setEmail(studentDto.getEmail());
+
+            // 수정된 내용을 DB에 저장
+            Student updatedStudent = studentRepository.save(student);
+
+            // 수정된 객페를 DTO로 변환하여 반환
+            return new StudentDto(updatedStudent.getId()
+                , updatedStudent.getName()
+                , updatedStudent.getEmail()
+            );
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    "Error occurred while updating student", e
+            );
+        }
+    }
+    //5 특정 ID 학생 삭제
+    public void deleteStudent(Long id) {
+        try {
+            // 삭제할 학생 데이터를 ID로 조회
+            Student student = studentRepository.findById(id)
+                .orElseThrow(() ->
+                        new Error("Student found with id" + id)
+                );
+            // 조회한 학생 객체를 DB에서 삭제
+            studentRepository.delete(student);
+        }catch (Exception e) {
+            throw new ResponseStatusException(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "Error occurred while deleting student", e
+            );
         }
     }
 }
